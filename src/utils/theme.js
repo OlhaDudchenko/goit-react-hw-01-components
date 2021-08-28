e0380b4a28c713ac5c81fc05657a638f21535b7b
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-08-29 02:51:36 +0300

    add utils

---
+++ src/utils/theme.js
@@ -0,0 +1,15 @@
+export const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    green: '#4caf50',
+    HotPink: '#FF69B4',
+    blue: '#2196f3',
+    secondaryText: '#757575',
+    SkyBlue: '#87CEEB',
+    DarkOrchid: '#9932CC',
+    LightSalmon: '#FFA07A',
+    FireBrick: '#B22222',
+    LightGrey: '#D3D3D3',
+  },
+});
